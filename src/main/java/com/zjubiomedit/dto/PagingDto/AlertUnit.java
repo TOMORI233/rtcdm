7c180892014cddfefb3a85c95a44e96acba73120
TOMORI <ihatewo@qq.com>
2020-05-05 17:14:59 +0800

    timeFormat

--- src/main/java/com/zjubiomedit/dto/PagingDto/AlertUnit.java
+++ src/main/java/com/zjubiomedit/dto/PagingDto/AlertUnit.java
@@ -1,5 +1,6 @@
 package com.zjubiomedit.dto.PagingDto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,10 +12,12 @@ public class AlertUnit {
     private String alertName;
     private String alertReason;
     private String alertMessage;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date alertTime;
     private Integer status;
     private Long followUpSerialNo;
     private String ignoreReason;
     private Long executeDoctorID;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date executeTime;
 }
